bb608d05df16372d701d47a98915ff26e543e2f0
Jake <jake.arefyev@gmail.com>
2017-04-27 21:10:18 +0200

    7 am > 6 am

--- apps/cron.js
+++ apps/cron.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cron = require(path.resolve('lib', 'schedule.js'));
 
 module.exports = function(applecation){
-    cron.schedule("1 0 7 * * *", function resetAllGameTimers(options){
+    cron.schedule("1 0 6 * * *", function resetAllGameTimers(options){
         applecation.db.resetAllGameTimers();
     });
 };
